bb1856e59dc0c19260b4f0f3c37ab7b4ade5aafd
antonio <antoniog.web@gmail.com>
2023-11-13 14:49:42 +0100

    pulizia

--- Application/Views/Utenti/login.php
+++ Application/Views/Utenti/login.php
@@ -5,11 +5,7 @@
 	<div>
 		<div></div>
 		
-		<!--<h3>Welcome to Articms</h3>
-		<p>Login in. To see it in action.</p>-->
-		
 		<p>
-<!-- 			<img width="100px" src="<?php echo $this->baseUrl."/Public/Img/logo_nsl.png"?>" /> -->
 			<br /><br />
 			<span class="errorLogin"></span>
 			<br />
@@ -28,20 +24,6 @@
 			</div>
 			
 			<button class="btn btn-primary btn-block" type="submit">Login</button>
-			
-<!-- 			<br /> -->
-<!-- 			<a href="javascript: void($('.recuperaPsw').slideToggle());"><small>Recupera la password</small></a> -->
 		</form>
-		
-		<!--<form class="m-t recuperaPsw hiddenBlock" role="form" action="" method="post">
-			<input type="hidden" name="recuperaPassword" value="recuperaPassword" />
-			
-			<div class="form-group">
-				<input type="email" class="form-control" placeholder="E-mail" required="" name="email" value="">
-			</div>
-			<button type="submit" class="btn btn-primary block full-width m-b">Recupera</button>
-		</form>-->
-					
-<!-- 		<p class="m-t"> <small>ArtiCMS 3.0</small> </p> -->
 	</div>
 </div>
